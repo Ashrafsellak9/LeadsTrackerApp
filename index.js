e5e7a9ca17a2e8e854d6226bb47bce726a3267fb
Ashrafsellak9 <sellakachraf9@gmail.com>
2026-09-15 14:32:46 +0100

    add Firebase config real time data

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-database.js"
+
+const firebaseConfig = {
+  databaseURL: process.env.DATABASE_URL
+}
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+console.log(database);
+console.log(app)
+console.log(firebaseConfig.databaseURL)
+
+
 let inputEl = document.getElementById("input-el");
 let inputBtn = document.getElementById("input-btn");
 let ulEl = document.getElementById("ul-el");
-
+let deleteBtn = document.getElementById("delete-btn");
 let myLeads = []
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if(leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
+deleteBtn.addEventListener("dblclick", () => {
+  localStorage.clear();
+  myLeads = [];
+  renderLeads();
+})
 
 inputBtn.addEventListener("click", () => {
     myLeads.push(inputEl.value);
-    renderLeads();
     inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    renderLeads();
+    console.log(localStorage.getItem("myLeads"));
 })
 
-const renderLeads = () => {
+const render = (leads) => {
     let listItems = "";
-    for(let i = 0; i < myLeads.length; i++) {
+    for(let i = 0; i < leads.length; i++) {
         listItems += `
           <li>
-            <a target="_blank" href="${myLeads[i]}">${myLeads[i]}</a>
+            <a target="_blank" href="${leads[i]}">${leads[i]}</a>
           </li>
         `;
     }
     ulEl.innerHTML = listItems;
 }
+
+render(myLeads);
